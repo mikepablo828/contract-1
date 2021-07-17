6a11ea702b3aeca08b540d4d01c084548b53b828
allmysmarts <rockid332@gmail.com>
2021-07-17 15:12:07 +0900

    integrate infura

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,17 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const mnemonic = "sample mnemonic here located :)";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/<INFURA-PROJECT-ID>")
+      },
+      network_id: 3
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
